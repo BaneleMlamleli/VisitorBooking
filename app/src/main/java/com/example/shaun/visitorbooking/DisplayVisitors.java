6a6058648088a214d0d0f6e52b9ef891224b7e81
BaneleMlamleli <mlamlelibanele@yahoo.com>
2017-11-22 01:04:59 +0200

    Created navigation sidebar, however it does not respond yet when you click on an option in the sidebar. This in turn disallows opening of other intents based on the clicked option on the sidebar.

---
+++ app/src/main/java/com/example/shaun/visitorbooking/DisplayVisitors.java
@@ -0,0 +1,13 @@
+package com.example.shaun.visitorbooking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayVisitors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_visitors);
+    }
+}
