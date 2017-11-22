f3dade716c1a168ba7e63e11ec8fccedc1bc75cd
BaneleMlamleli <mlamlelibanele@yahoo.com>
2017-11-22 02:30:47 +0200

    Matched code with  the CPUT App code

--- app/src/main/java/com/example/shaun/visitorbooking/MainActivity.java
+++ app/src/main/java/com/example/shaun/visitorbooking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shaun.visitorbooking;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,15 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.*;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout menuLayout;
-    private ActionBarDrawerToggle toggleBar;
+    private ActionBarDrawerToggle toggleBar;    //This is the button that will be pressed to show the hidden menu
+    private MenuItem myMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +34,23 @@ public class MainActivity extends AppCompatActivity {
         toggleBar.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         final NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 if (id == R.id.registerVisitor) {
-                    Toast.makeText(MainActivity.this, "Register visitor", Toast.LENGTH_SHORT).show();
-                    Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(mainActivityIntent);
+                    Toast.makeText(getApplicationContext(), "Register visitor", Toast.LENGTH_SHORT).show();
+                    Intent registerVisitorIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(registerVisitorIntent);
                     //Changing the title of the bar according to the selected option
                     getSupportActionBar().setTitle("Register visitor");
                 } else {
                     if (id == R.id.displayVisitors) {
-                        Toast.makeText(getApplicationContext(), "Display visitor", Toast.LENGTH_SHORT).show();
-                        Intent displayVisitorIntent = new Intent(getApplicationContext(), DisplayVisitors.class);
-                        startActivity(displayVisitorIntent);
+                        Toast.makeText(getApplicationContext(), "Display visitors", Toast.LENGTH_SHORT).show();
+                        Intent displayVisitorsIntent = new Intent(getApplicationContext(), DisplayVisitors.class);
+                        startActivity(displayVisitorsIntent);
                         //Changing the title of the bar according to the selected option
                         getSupportActionBar().setTitle("Display visitors");
                     }
