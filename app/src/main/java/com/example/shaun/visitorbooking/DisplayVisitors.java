f5ade0bba066bcaeee3ceddd0f99ad4ed23570d7
BaneleMlamleli <mlamlelibanele@yahoo.com>
2017-11-23 01:37:23 +0200

    Created an adapter for the spinner and populated a spinner with dummy values. ArrayList will contain all the file names.

--- app/src/main/java/com/example/shaun/visitorbooking/DisplayVisitors.java
+++ app/src/main/java/com/example/shaun/visitorbooking/DisplayVisitors.java
@@ -2,6 +2,11 @@ package com.example.shaun.visitorbooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DisplayVisitors extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class DisplayVisitors extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_visitors);
+
+        //
+        Spinner dateSpinner = findViewById(R.id.snpSelectDate);
+        String date;
+
+        // Adding dates for the date spinner
+        ArrayList<Integer> datesSpinnerArray = new ArrayList<>();
+        for (int i = 1; i <= 31; i++) {
+            datesSpinnerArray.add(i);
+        }
+
+        ArrayAdapter<Integer> datesArrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, datesSpinnerArray);
+
+        datesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateSpinner.setAdapter(datesArrayAdapter);
+        date = dateSpinner.getSelectedItem().toString();
+        Toast.makeText(getApplicationContext(), date + " was selected", Toast.LENGTH_SHORT).show();
     }
 }
