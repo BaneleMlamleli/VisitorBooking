72f156a0c8ddd8b3f3ad691102fee39b0c734138
BaneleMlamleli <mlamlelibanele@yahoo.com>
2017-11-24 04:31:33 +0200

    94 percent working

--- app/src/main/java/com/example/shaun/visitorbooking/DisplayVisitors.java
+++ app/src/main/java/com/example/shaun/visitorbooking/DisplayVisitors.java
@@ -2,35 +2,149 @@ package com.example.shaun.visitorbooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DisplayVisitors extends AppCompatActivity {
+
+    private TextView txtDisplayVisitors;
+    private Button btnDisplayVisitors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_visitors);
 
+        //Enabling TextView field to be scrollable
+        txtDisplayVisitors = (TextView) findViewById(R.id.txtDisplayVisitors);
+        txtDisplayVisitors.setMovementMethod(new ScrollingMovementMethod());
+        btnDisplayVisitors = (Button) findViewById(R.id.btnDisplayVisitors);
+
         //
-        Spinner dateSpinner = findViewById(R.id.snpSelectDate);
+        final Spinner dateSpinner = findViewById(R.id.snpSelectDate);
         String date;
+        //============================
+
+        String fullDate;
+    /*The below code will generate today's date in the format dd-month-yyyy e.g., 22-November-2017
+     * This code is for naming the txt file with the current date. e.g., 22-November-2017.txt
+     * */
+        String myDate = DateFormat.getDateInstance(DateFormat.LONG).format(new Date());
+        String dd = myDate.substring(0, 2);
+        String month = myDate.substring(3, (myDate.length() - 5));
+        String year = myDate.substring((myDate.length() - 4), myDate.length());
+
+        //If the date is a single digit it is going to add a zero to make it like this, '01'
+        if (dd.length() == 1) {
+            dd = "0" + dd;
+            fullDate = dd + "-" + month + "-" + year;
+        } else {
+            fullDate = dd + "-" + month + "-" + year;
+        }
+        final String dataFile = fullDate + ".txt";
+
+        //============================
+
+        boolean exst = true;
+        ArrayList<String> fileNames = new ArrayList<>();
+        int index = 0;
+        String newDataFile = dataFile;
+        do {
+            int dt = Integer.parseInt(newDataFile.substring(0, 2));
+            File file = new File(newDataFile);
+            if (file.exists()) {
+                //System.out.println("file is already there");
+                dt -= 1;
+                newDataFile = dt + newDataFile.substring(2, newDataFile.length());
+                fileNames.add(index, newDataFile);
+                index += 1;
+                exst = true;
+            } else {
+                exst = false;
+                //System.out.println("Not find file ");
+            }
+        } while (exst != false);
+
+
+        //============================
 
         // Adding dates for the date spinner
-        ArrayList<Integer> datesSpinnerArray = new ArrayList<>();
-        for (int i = 1; i <= 31; i++) {
-            datesSpinnerArray.add(i);
+        ArrayList<String> datesSpinnerArray = new ArrayList<>();
+        for (int i = 0; i <= fileNames.size(); i++) {
+            datesSpinnerArray.add(i, fileNames.get(i).toString());
         }
 
-        ArrayAdapter<Integer> datesArrayAdapter = new ArrayAdapter<>(
+        ArrayAdapter<String> datesArrayAdapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_spinner_item, datesSpinnerArray);
 
         datesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dateSpinner.setAdapter(datesArrayAdapter);
-        date = dateSpinner.getSelectedItem().toString();
-        Toast.makeText(getApplicationContext(), date + " was selected", Toast.LENGTH_SHORT).show();
+
+        btnDisplayVisitors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fileName = dateSpinner.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(), fileName, Toast.LENGTH_SHORT).show();
+
+
+                //====================
+
+                try {
+                    FileInputStream fileIn = openFileInput(dataFile);
+                    InputStreamReader InputRead = new InputStreamReader(fileIn);
+
+                    final int READ_BLOCK_SIZE = 1000;
+
+                    char[] inputBuffer = new char[READ_BLOCK_SIZE];
+                    String s = "";
+                    int charRead;
+
+                    while ((charRead = InputRead.read(inputBuffer)) > 0) {
+                        // char to string conversion
+                        String readstring = String.copyValueOf(inputBuffer, 0, charRead);
+                        s += readstring;
+                    }
+                    txtDisplayVisitors.setText(s);
+                    Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                    InputRead.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                //====================
+                /*
+                File file = new File(Environment.getExternalStorageDirectory(), "22-November.txt");//fileName);
+                try{
+                    int len = (int) file.length();
+                    byte[] bytes = new byte[len];
+
+                    FileInputStream readFile = new FileInputStream(file);
+                    readFile.read(bytes);
+
+                    String readDate = new String(bytes);
+                    txtDisplayVisitors.setText(readDate.toString());
+                    readFile.close();
+                    Toast.makeText(getApplicationContext(), "File read succussfully!", Toast.LENGTH_SHORT).show();
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error file not found!", Toast.LENGTH_SHORT).show();
+                }*/
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shaun/visitorbooking/MainActivity.java
+++ app/src/main/java/com/example/shaun/visitorbooking/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.shaun.visitorbooking;
 
 import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.io.*;
 
 import org.w3c.dom.Text;
 
@@ -30,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         EditText contactNumber = findViewById(R.id.txtContactNumber);
         EditText personVisitingNumber = findViewById(R.id.txtPersonVisitingNumber);
         final EditText reason = findViewById(R.id.txtReason);
+
+        /* Allowing the EditText field to be scrollable */
+        reason.setMovementMethod(new ScrollingMovementMethod());
 
         final String name = visitorName.getText().toString();
         final String persnVisitingNum = personVisitingNumber.getText().toString();
@@ -74,8 +84,55 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        writeToFile(name, persnVisitingNum, contctNum, reasn);
-                        Toast.makeText(getApplicationContext(), "Visitor registered", Toast.LENGTH_SHORT).show();
+                        String fullDate;
+                        /*The below code will generate today's date in the format dd-month-yyyy e.g., 22-November-2017
+                        * This code is for naming the txt file with the current date. e.g., 22-November-2017.txt
+                        * */
+                        String myDate = DateFormat.getDateInstance(DateFormat.LONG).format(new Date());
+                        String dd = myDate.substring(0, 2);
+                        String month = myDate.substring(3, (myDate.length() - 5));
+                        String year = myDate.substring((myDate.length() - 4), myDate.length());
+
+                        //If the date is a single digit it is going to add a zero to make it like this, '01'
+                        if (dd.length() == 1) {
+                            dd = "0" + dd;
+                            fullDate = dd + "-" + month + "-" + year;
+                        } else {
+                            fullDate = dd + "-" + month + "-" + year;
+                        }
+                        String text = "Visitor: " + name + "\nPerson visiting: " + persnVisitingNum +
+                                "\nVisitor number: " + contctNum + "\nReason: " + reasn + "\n======================";
+                        String fileName = fullDate + ".txt";
+
+                        //================
+
+                        try {
+                            FileOutputStream fileout = openFileOutput(fileName, MODE_PRIVATE);
+                            OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
+                            outputWriter.append(text.toString());
+                            //outputWriter.write(text.toString());
+                            outputWriter.close();
+
+                            //display file saved message
+                            Toast.makeText(getBaseContext(), "File saved successfully!",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        //================
+
+                        /*File file = new File(Environment.getExternalStorageDirectory(), fileName);
+                        try{
+                            FileOutputStream fileOutput = new FileOutputStream(file);
+                            fileOutput.write(text.toString().getBytes());
+                            fileOutput.close();
+                            Toast.makeText(getApplicationContext(), "Visitor registered", Toast.LENGTH_SHORT).show();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Error writing to file "+fileName, Toast.LENGTH_SHORT).show();
+                        }*/
                     }
                 }
         );
@@ -84,9 +141,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return toggleBar.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
-    }
-
-    public void writeToFile(String nm, String perVisitingNum, String contNum, String rsn) {
-        Toast.makeText(getApplicationContext(), "Button clicked, " + nm + ", xx", Toast.LENGTH_SHORT).show();
     }
 }
